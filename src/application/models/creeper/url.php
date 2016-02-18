dd1931d5b2b1d1f4806083b5828fc7bd00bf5dfe
chchmlml <chchmlml@163.com>
2016-02-18 13:56:29 +0800

    remove log

--- src/application/models/creeper/url.php
+++ src/application/models/creeper/url.php
@@ -1,4 +1,5 @@
 <?php
+use Snoopy\Snoopy;
 
 /**
  * Class Url_model
@@ -19,7 +20,14 @@ class url extends CI_Model
      */
     public function init($start_url = '')
     {
-        log_message('error', 'Some variable did not contain a value.');
+        $snoopy = new Snoopy;
+        $snoopy->proxy_port = "80";
+        $snoopy->agent = "(compatible; MSIE 4.01; MSN 2.5; AOL 4.0; Windows 98)";
+        $snoopy->rawheaders["Pragma"] = "no-cache"; //cache 的http头信息
+        $snoopy->read_timeout = 10;
+        $snoopy->fetchlinks($start_url);
+
+        log_message('debug', var_export($snoopy->results, true));
         exit;
         return 'hello model.';
     }
