d563dadaf10aec9fca371b45a27d1d887c7b5a84
haicheng <haicheng@staff.weibo.com>
2016-02-22 11:03:19 +0800

    add comment

--- src/application/models/creeper/downloader.php
+++ src/application/models/creeper/downloader.php
@@ -29,13 +29,16 @@ class downloader extends CI_Model
             return 0;
         }
         if (!empty($urls)) {
+            //将数组分组，分批下载
             $urls_array = array_chunk($urls, $this->rows);
             foreach($urls_array as $array_key => $urls_item){
                 echo "正在下载第" . ($this->rows * $array_key + 1) . "~" . ($this->rows * ($array_key + 1)) . "的页面..." . "\n";
                 $urls_item_val = array_column($urls_item, 'url');
                 $urls_key = array_column($urls_item, 'url_md5');
+                //下载
                 $this->multicurlclass->set_urls($urls_item_val);
                 $data = $this->multicurlclass->start();
+                //通过md5保存值
                 foreach ($urls_key as $key => $urls_key_val) {
                     if (!file_exists($this->path . $urls_key_val . '.html')) {
                         write_file($this->path . $urls_key_val . '.html', iconv('gbk', 'utf-8//IGNORE', $data[$key]));
