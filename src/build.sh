f8c59aede3d2e6436acb5ea369f499607b315a07
haicheng <haicheng@staff.weibo.com>
2016-05-27 14:39:08 +0800

    build。sh

--- src/build.sh
+++ src/build.sh
@@ -2,6 +2,7 @@
 
 set -x
 
+# ----- 帮助信息 -----
 show_help_and_exit() {
 cat <<HELP
 stage param must be specified
@@ -15,8 +16,14 @@ if [ $# -lt 1 ]; then
     show_help_and_exit;
 fi
 
-#psr0 规范格式化代码
-PHP_CS_FIXER=/Users/haicheng/developer/wwwroot/ceshi/src/shell/php-cs-fixer.phar
-PROJECT_PATH=/Users/haicheng/developer/wwwroot/ceshi/src/application/
+# ----- 项目地址 -----
+PROJECT_PATH=${PWD}/
 
-php ${PHP_CS_FIXER} fix ${PROJECT_PATH} --level=psr0
+# ----- psr0规范格式化代码 -----
+PHP_CS_FIXER=${PROJECT_PATH}/shell/php-cs-fixer.phar
+php ${PHP_CS_FIXER} fix ${PROJECT_PATH}/application/ --level=psr0
+
+# ----- 测试用例运行 -----
+PHPUNIT=${PROJECT_PATH}/shell/phpunit-4.8.24.phar
+TEST_PATH=${PROJECT_PATH}/tests
+PHPUNIT ${TEST_PATH} --configuration ${TEST_PATH}/phpunit.xml
