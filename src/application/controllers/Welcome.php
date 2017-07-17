161e34b3edffc9612099d5f195c2ad3e94591778
崔海程 <haicheng@cuihaichengdeMacBook-Pro-2.local>
2017-07-17 09:37:39 +0800

    fi

--- src/application/controllers/Welcome.php
+++ src/application/controllers/Welcome.php
@@ -7,12 +7,13 @@ class Welcome extends CI_Controller {
 
     public function index() {
         echo 'give a test';
+        exit;
         //$this->load->view('welcome/index');
-        $path = APPPATH . 'controllers/Tools.php';
-        if (file_exists($path)) {
-            apc_clear_cache();
-            Apc::cache_file($path);
-            echo 'cache it';
-        }
+//        $path = APPPATH . 'controllers/Tools.php';
+//        if (file_exists($path)) {
+//            apc_clear_cache();
+//            Apc::cache_file($path);
+//            echo 'cache it';
+//        }
     }
 }
