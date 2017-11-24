5b57b6b218359b7066b90ec62166b7bb315a8ac2
崔海程 <haicheng@cuihaichengdeMacBook-Pro-2.local>
2017-11-24 16:00:02 +0800

    fix ...

--- src/test_faker.php
+++ src/test_faker.php
@@ -5,10 +5,12 @@
 require 'vendor/autoload.php';
 
 $faker = Faker\Factory::create('zh_CN');
-//echo $faker->firstName('cui');
-//echo $faker->name;
+echo $faker->firstName('cui');
+for ($i = 0; $i < 20; $i++) {
+	echo $faker->name, "\n";
+}
 
-echo $faker->company;
+//echo $faker->company;
 //echo $faker->uuid;
 
 //echo $faker->randomHtml(2);
