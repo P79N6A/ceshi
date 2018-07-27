b659140619e22f43019dd6982bb3f9a4c5ea639d
haicheng <haicheng@staff.weibo.com>
2018-07-27 13:56:31 +0800

    fix import script

---
+++ src/111.php
@@ -0,0 +1,14 @@
+{'creative_name':'测试2222','start_time':'2018-04-30','end_time':'2018-04-30','remark':'说明一下','url':'http://baidu.com','content':'hahahaha','monitor_type':1,'monitor_company':'','third_party_show':'','third_party_click':'','attachment':''}3
+
+
+2011-07/2013-09
+公司：爱奇艺PPS网络视频
+部门：广告事业部
+
+2013-10/2015-06
+公司：
+部门：O2O事业部
+
+2015-07/NOW
+公司：
+部门：商业产品广告组
